831c25416d5d9721e27c14608de0d29b7da417e5
Apple <apple@Apples-MacBook-Pro.local>
2020-01-02 12:56:48 +0530

    defaultSpringBootSecurity

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,35 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.demo.entity.Employee;
-import com.example.demo.model.UserDetails;
-import com.example.demo.service.IEmployeeService;
 
 @Controller
 public class SampleController {
 	
-	private UserDetails userDetails;
-	
-	@Autowired
-	private IEmployeeService employeeService;
-	
-
-	@Autowired
-	public void setUserDetails(UserDetails userDetails) {
-		this.userDetails = userDetails;
-	}
 
 	@GetMapping("/hello")
 	@ResponseBody
-	public UserDetails sampleGreet() {
-		return userDetails;
+	public String sampleGreet() {
+		return "Welcome";
 	}
 	
 	@GetMapping("/user")
@@ -43,18 +25,5 @@ public class SampleController {
 	public String adminlogin() {
 		return "admin logged in";
 	}
-	
-	@GetMapping("/getemployee/{id}")
-	@ResponseBody
-	public String getEmployee(@PathVariable("id") String id) {
-		Employee empl = null;
-		Long employeeId = Long.parseLong(id);
-		Optional<Employee> emp = employeeService.getEmployee(employeeId);
-		if(emp.isPresent()){
-			empl = emp.get();
-		}
-		return empl.toString();
-	}
-	
 	
 }
