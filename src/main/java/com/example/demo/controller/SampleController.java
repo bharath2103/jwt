523a8c88960f17631e1bc091728caaf1a985af7a
Apple <apple@Apples-MacBook-Pro.local>
2020-01-06 08:57:17 +0530

    authenticationusingjpaandjwt

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.entity.User;
+import com.example.demo.entity.MyUserDetails;
+import com.example.demo.model.AuthenticationRequest;
+import com.example.demo.model.AuthenticationResponse;
+import com.example.demo.security.MyUserDetailService;
+import com.example.demo.security.util.JwtUtil;
 import com.example.demo.service.UserService;
 
 @Controller
@@ -18,10 +25,37 @@ public class SampleController {
 	@Autowired
 	private UserService userservice;
 	
+	@Autowired
+	private MyUserDetailService userDetailService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+	
 	@ResponseBody
 	@GetMapping("/hello")
 	public String sampleGreet() {
 		return "Welcome";
+	}
+	
+	@ResponseBody
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception{
+		try {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		}
+		catch(BadCredentialsException ex) {
+			throw new Exception("Incorrect Username and Password" + ex);
+		}
+		
+		final UserDetails userDetails = userDetailService.loadUserByUsername(authenticationRequest.getUsername());
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 	}
 
 	/*@ResponseBody
--- src/main/java/com/example/demo/security/MySecurity.java
+++ src/main/java/com/example/demo/security/MySecurity.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,19 +21,24 @@ public class MySecurity extends WebSecurityConfigurerAdapter{
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailService);
 	}
-
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+	
+	}
+	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/**")
-		.hasRole("ADMIN")
-		.and()
-		.formLogin();
-	
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
